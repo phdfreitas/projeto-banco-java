977658caaf3f448e11ca4b95bafb908df3ab37a5
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-01-15 10:27:26 -0300

    Introdução ao Pilar do Encapsulamento

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -2,6 +2,13 @@ package entidades;
 
 public class Cliente {
 
-    public String nome;
+    private String nome;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/entidades/ContaCorrente.java
+++ src/entidades/ContaCorrente.java
@@ -2,9 +2,21 @@ package entidades;
 
 public class ContaCorrente {
     
-    public int agencia;
-    public int numero;
-    public double saldo;
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 
     public void sacar(){}
 
